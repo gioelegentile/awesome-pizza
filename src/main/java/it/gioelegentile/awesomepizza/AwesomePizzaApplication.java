4ba717455bd0c8c5f2d40b376e8bcc56a982b554
Gioele Gentile <gioelegentile.it@gmail.com>
2023-04-10 22:18:31 +0200

    initial commit

---
+++ src/main/java/it/gioelegentile/awesomepizza/AwesomePizzaApplication.java
@@ -0,0 +1,13 @@
+package it.gioelegentile.awesomepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomePizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomePizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/gioelegentile/awesomepizza/AwesomePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gioelegentile.awesomepizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomePizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
